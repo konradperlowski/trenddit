ecac0f2b71eb14b4caa9b8321cae21366ce0cc7a
Silnari <konrad.perl@gmail.com>
2020-10-09 10:45:43 +0200

    Added home page with common navbar and common header tag

---
+++ src/main/java/trenddit/controller/HomeController.java
@@ -0,0 +1,14 @@
+package trenddit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String home(Model model) {
+        return "home";
+    }
+}
