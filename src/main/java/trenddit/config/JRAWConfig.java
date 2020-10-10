d20b3e702be941f762b1f65a8a6730de08870a0a
Silnari <konrad.perl@gmail.com>
2020-10-10 20:29:20 +0200

    Simplification in JRAWConfig file

--- src/main/java/trenddit/config/JRAWConfig.java
+++ src/main/java/trenddit/config/JRAWConfig.java
@@ -16,18 +16,13 @@ public class JRAWConfig {
 
     @Bean
     public RedditClient getReddit() {
-        return reddit;
-    }
-
-    private final RedditClient reddit;
-
-    public JRAWConfig() {
-        UserAgent userAgent = new UserAgent("bot", "trenddit", "v0.1", "Trenddit");
-        Credentials credentials = Credentials.userless(
-                "UiiCHPMPwVJozA",
-                "AblwWj2rKosVoIflCrpgONjfMEQ",
-                new UUID(2, 3));
-        NetworkAdapter adapter = new OkHttpNetworkAdapter(userAgent);
-        reddit = OAuthHelper.automatic(adapter, credentials);
+        return OAuthHelper.automatic(
+                new OkHttpNetworkAdapter(new UserAgent("bot",
+                        "trenddit",
+                        "v0.1",
+                        "Trenddit")),
+                Credentials.userless("UiiCHPMPwVJozA",
+                        "AblwWj2rKosVoIflCrpgONjfMEQ",
+                        new UUID(2, 3)));
     }
 }
