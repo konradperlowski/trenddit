b5b0a65ce8fe37eaacca86471b21344af7531c63
Silnari <konrad.perl@gmail.com>
2020-10-12 16:23:00 +0200

    Improved logging when executing python scripts

--- src/main/java/trenddit/cron/UpdateDBCron.java
+++ src/main/java/trenddit/cron/UpdateDBCron.java
@@ -5,9 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.io.InputStreamReader;
 
 @Component
 public class UpdateDBCron {
@@ -18,8 +18,6 @@ public class UpdateDBCron {
     public void updateDB() throws InterruptedException, IOException {
         log.info("Update db started...");
 
-        String currentDate = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
-
         String[] command = new String[3];
 
         if (System.getProperty("os.name").startsWith("Windows")) {
@@ -29,12 +27,42 @@ public class UpdateDBCron {
             command[0] = "/bin/bash";
             command[1] = "-c";
         }
-        command[2] = "python3 scripts/src/update_data.py > scripts/" + currentDate + "-out.log";
+        command[2] = "python3 scripts/src/update_data.py";
 
         Process p = Runtime.getRuntime().exec(command);
+        logCommandOut(p);
 
         int exitCode = p.waitFor();
         if (exitCode == 0) log.info("Update ended successfully");
-        else log.info("Update failed, please check script/" + currentDate + "-out.log for more info");
+        else log.error("Update failed");
+    }
+
+    private void logCommandOut(Process p) throws IOException {
+        BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+
+        log.info("Standard output of the command:");
+        StringBuilder standardOut = new StringBuilder();
+        StringBuilder errorOut = new StringBuilder();
+
+        String outLine;
+        while ((outLine = stdInput.readLine()) != null) {
+            standardOut.append(outLine);
+            standardOut.append('\n');
+        }
+
+        if (!standardOut.toString().equals("")) {
+            log.info("Output of db update:");
+            log.info(standardOut.toString());
+        }
+        while ((outLine = stdError.readLine()) != null) {
+            errorOut.append(outLine);
+            errorOut.append('\n');
+        }
+
+        if (!errorOut.toString().equals("")) {
+            log.error("Error while updating db:");
+            log.error(errorOut.toString());
+        }
     }
 }
