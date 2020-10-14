4dc5f5ce8181ae5f46602d635df5e20b159d0cff
Silnari <konrad.perl@gmail.com>
2020-10-14 18:31:21 +0200

    Added loading spinner on tables load

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
+    let loading = $('#loadingDiv');
+    let content = $('#pageContent');
+    loading.css('display', 'block');
+    content.css('display', 'none')
+
     $('#subscribersTable').DataTable();
     $('#growthTableDay').DataTable();
     $('#growthTableWeek').DataTable();
     $('#growthTableMonth').DataTable();
+
+    loading.css('display', 'none');
+    content.css('display', 'block')
 });
