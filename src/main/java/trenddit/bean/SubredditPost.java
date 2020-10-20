206d27d54105d26fb416b261b6d50d283a59af6f
Silnari <konrad.perl@gmail.com>
2020-10-20 11:09:49 +0200

    Change submission number to number with suffix

--- src/main/java/trenddit/bean/SubredditPost.java
+++ src/main/java/trenddit/bean/SubredditPost.java
@@ -3,12 +3,20 @@ package trenddit.bean;
 public class SubredditPost {
     private String title;
     private String url;
-    private Integer upVotes;
+    private String upVotes;
 
     public SubredditPost(String title, String url, Integer upVotes) {
         this.title = title.replaceAll("(?<=.{45})\\b.*", "...");
         this.url = "https://www.reddit.com" + url;
-        this.upVotes = upVotes;
+        this.upVotes = withSuffix(upVotes);
+    }
+
+    public static String withSuffix(int count) {
+        if (count < 1000) return "" + count;
+        int exp = (int) (Math.log(count) / Math.log(1000));
+        return String.format("%.1f %c",
+                count / Math.pow(1000, exp),
+                "kMGTPE".charAt(exp-1));
     }
 
     public String getTitle() {
@@ -27,11 +35,11 @@ public class SubredditPost {
         this.url = url;
     }
 
-    public Integer getUpVotes() {
+    public String getUpVotes() {
         return upVotes;
     }
 
-    public void setUpVotes(Integer upVotes) {
+    public void setUpVotes(String upVotes) {
         this.upVotes = upVotes;
     }
 }
